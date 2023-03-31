7936e05a488fd874a15051e1778bc6dc4bc23de0
Ummar Farooq <farooqcherur@gmail.com>
2023-04-01 00:25:35 +0530

    inside the QuizFormQuestions component added options and correct answer selector they change dynamically when admin add how much options they want for each question

--- front-end/src/components/adminside/QuizFormQuestions.jsx
+++ front-end/src/components/adminside/QuizFormQuestions.jsx
@@ -1,37 +1,25 @@
 import { useEffect, useState } from "react";
 
 export const QuizFormQuestions = () => {
-
   const [totalOption, setTotalOption] = useState(0);
-  const [options,setOptions]=useState([])
-
+  const [options, setOptions] = useState([]);
+  const [correctAnsOption, setCorrectAnsOption] = useState([]);
 
   const handleOptionsAccToNumber = () => {
-    
-    let optionArray=[]
+    const optionArray = Array.from({ length: totalOption }, (_, index) => {
+      return (
+        <label className="input-group input-group-vertical">
+          <span>option {index + 1}</span>
+          <input
+            type="text"
+            className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+          />
+        </label>
+      );
+    });
 
-       for(let i=0;i<=totalOption;i++){
-        
-        optionArray.push([
-
-            <label className="input-group input-group-vertical">
-            <span className="">option B</span>
-            <input
-              type="text"
-              className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </label>
-
-        ])
-       }
-    
-       setOptions([...options, ...optionArray]);
-
-
-    
+    setOptions(optionArray);
   };
-
-
 
   useEffect(() => {
     handleOptionsAccToNumber();
@@ -40,6 +28,20 @@ export const QuizFormQuestions = () => {
   const handleTotalOption = (event) => {
     setTotalOption(parseInt(event.target.value));
   };
+
+  const handleCorrectAnswerOptionSelect = () => {
+    const correctAnswerOptionArray = Array.from(
+      { length: options.length },
+      (item, index) => {
+        return <option>option {index + 1}</option>;
+      }
+    );
+    setCorrectAnsOption(correctAnswerOptionArray);
+  };
+
+  useEffect(() => {
+    handleCorrectAnswerOptionSelect();
+  }, [options]);
 
   return (
     <>
@@ -68,20 +70,35 @@ export const QuizFormQuestions = () => {
             <input
               type="number"
               onChange={handleTotalOption}
+              max={6}
+              min={1}
               className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
 
-          <div className="mt-7 w-4/5 flex flex-col items-center">
-            <label className="text-lg block font-semibold text-gray-800 mb-2">
-              Options
-            </label>
-
-            <div className="form-control">
-         
-         {options}
+          {options.length>0 && (
+            <div className="mt-7 w-4/5 flex flex-col items-center">
+              <label className="text-lg block font-semibold text-gray-800 mb-2">
+                Options
+              </label>
+              <div className="form-control">{options}</div>
             </div>
-          </div>
+          )}
+
+          {correctAnsOption.length > 0 && (
+            <div className="mt-7 w-4/5 flex flex-col items-center">
+              <label className="text-lg block font-semibold text-gray-800 mb-2">
+                Correct answer
+              </label>
+
+              <select className=" px-4 py-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                <option disabled selected>
+                  select
+                </option>
+                {correctAnsOption}
+              </select>
+            </div>
+          )}
 
           <div className="mt-5 w-4/5 flex flex-col items-center mb-3">
             <button className="btn btn-outline btn-sm">Add question</button>
